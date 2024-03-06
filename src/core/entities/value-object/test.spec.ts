b02c3e4dd71a416bf644e212520226020232e618
kielkow <matheuskiel@fiorifer.com.br>
2024-03-06 00:34:46 -0300

    feat: use unique entity id at entities

--- src/core/entities/value-object/test.spec.ts
+++ src/core/entities/value-object/test.spec.ts
@@ -14,6 +14,20 @@ describe('Entity: Value Object', () => {
         expect(valueObject1.equals(valueObject2)).toBe(true);
     });
 
+
+    it('should compare different values objects', () => {
+        class TestValueObject extends ValueObject<{ value: string }> {
+            constructor(props: { value: string }) {
+                super(props);
+            }
+        }
+
+        const valueObject1 = new TestValueObject({ value: 'test1' });
+        const valueObject2 = new TestValueObject({ value: 'test2' });
+
+        expect(valueObject1.equals(valueObject2)).toBe(false);
+    });
+
     it('should compare value object with undefined', () => {
         class TestValueObject extends ValueObject<{ value: string }> {
             constructor(props: { value: string }) {
