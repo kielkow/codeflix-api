b880bc154bc590bfebe213a7de4f5661ebe1a22a
kielkow <matheuskiel@fiorifer.com.br>
2024-03-05 23:41:59 -0300

    feat: gender entity

---
+++ src/domain/entities/gender/index.ts
@@ -0,0 +1,97 @@
+import crypto from 'node:crypto';
+
+export interface GenderProps {
+    id?: string;
+    name: string;
+    description: string;
+    active?: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+interface CreateGenderProps {
+    name: string;
+    description: string;
+    active?: boolean;
+}
+
+export class Gender {
+    private _id: string;
+    private _name: string;
+    private _description: string;
+    private _active: boolean;
+    private _createdAt: Date;
+    private _updatedAt?: Date;
+
+    protected constructor(props: GenderProps) {
+        this._id = props.id || crypto.randomUUID();
+        this._name = props.name;
+        this._description = props.description;
+        this._active = props.active || false;
+        this._createdAt = props.createdAt || new Date();
+        this._updatedAt = props.updatedAt;
+    }
+
+    static create(props: CreateGenderProps): Gender {
+        return new Gender(props);
+    }
+
+    get id(): string {
+        return this._id;
+    }
+    get name(): string {
+        return this._name;
+    }
+    get description(): string {
+        return this._description;
+    }
+    get active(): boolean {
+        return this._active;
+    }
+    get createdAt(): Date {
+        return this._createdAt;
+    }
+    get updatedAt(): Date | undefined {
+        return this._updatedAt;
+    }
+
+    private set name(value: string) {
+        this._name = value;
+        this.touch();
+    }
+    private set description(value: string) {
+        this._description = value;
+        this.touch();
+    }
+    private set active(value: boolean) {
+        this._active = value;
+        this.touch();
+    }
+
+    private touch() {
+        this._updatedAt = new Date();
+    }
+
+    public changeName(name: string): void {
+        this.name = name;
+    }
+    public changeDescription(description: string): void {
+        this.description = description;
+    }
+    public activate(): void {
+        this.active = true;
+    }
+    public deactivate(): void {
+        this.active = false;
+    }
+    public toObject(): GenderProps {
+        return {
+            id: this._id,
+            name: this._name,
+            description: this._description,
+            active: this._active,
+            createdAt: this._createdAt,
+            updatedAt: this._updatedAt,
+        };
+    }
+}
---
+++ src/domain/entities/gender/test.spec.ts
@@ -0,0 +1,45 @@
+import { Gender } from ".";
+
+describe('Entity: Gender', () => {
+    it('should create a new gender', () => {
+        const gender = Gender.create({
+            name: 'Gender 1',
+            description: 'Gender 1 description',
+        });
+
+        expect(gender).toBeInstanceOf(Gender);
+        expect(gender.id).toBeDefined();
+        expect(gender.name).toBe('Gender 1');
+        expect(gender.description).toBe('Gender 1 description');
+        expect(gender.active).toBe(false);
+        expect(gender.createdAt).toBeInstanceOf(Date);
+        expect(gender.updatedAt).toBeUndefined();
+    });
+
+    it('should update gender', () => {
+        const gender = Gender.create({
+            name: 'Gender 1',
+            description: 'Gender 1 description',
+        });
+
+        gender.changeName('Gender 2');
+        gender.changeDescription('Gender 2 description');
+        gender.activate();
+
+        expect(gender.name).toBe('Gender 2');
+        expect(gender.description).toBe('Gender 2 description');
+        expect(gender.active).toBe(true);
+        expect(gender.updatedAt).toBeInstanceOf(Date);
+    });
+
+    it('should deactivate gender', () => {
+        const gender = Gender.create({
+            name: 'Gender 1',
+            description: 'Gender 1 description',
+        });
+
+        gender.deactivate();
+
+        expect(gender.active).toBe(false);
+    });
+});
