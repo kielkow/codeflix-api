164aa5422a3e70f936085ca730d4231df9259ad2
kielkow <matheuskiel@fiorifer.com.br>
2024-03-06 00:23:35 -0300

    feat: value object class entity

---
+++ src/core/entities/value-object/test.spec.ts
@@ -0,0 +1,28 @@
+import { ValueObject } from ".";
+
+describe('Entity: Value Object', () => {
+    it('should compare value object', () => {
+        class TestValueObject extends ValueObject<{ value: string }> {
+            constructor(props: { value: string }) {
+                super(props);
+            }
+        }
+
+        const valueObject1 = new TestValueObject({ value: 'test' });
+        const valueObject2 = new TestValueObject({ value: 'test' });
+
+        expect(valueObject1.equals(valueObject2)).toBe(true);
+    });
+
+    it('should compare value object with undefined', () => {
+        class TestValueObject extends ValueObject<{ value: string }> {
+            constructor(props: { value: string }) {
+                super(props);
+            }
+        }
+
+        const valueObject1 = new TestValueObject({ value: 'test' });
+
+        expect(valueObject1.equals(undefined)).toBe(false);
+    });
+});
--- src/domain/entities/gender/index.ts
+++ src/domain/entities/gender/index.ts
@@ -1,4 +1,4 @@
-import { UniqueEntityID } from '../unique-entity-id';
+import { UniqueEntityID } from '../../../core/entities/unique-entity-id';
 
 export interface GenderProps {
     id?: string;
